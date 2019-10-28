6ac04ad0c5837b6f0dea0d13f094b8628035b7e1
Austin O'Boyle <14arob@queensu.ca>
2019-10-27 23:04:16 -0700

    Create simple list and detail views
    - RequestList creates a new request, and lists the past 10
    - RequestDetails gives details of a specific request

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from api.models import Request
+from api.serializers import RequestSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class RequestList(APIView):
+    def get(self, request, format=None):
+        new_request = Request(request_type=request.method)
+        new_request.save()
+        requests = Request.objects.all()[:10]
+        serializer = RequestSerializer(requests, many=True)
+        return Response(serializer.data)
+
+
+class RequestDetails(APIView):
+    def get(self, request, pk):
+        r = get_object_or_404(Request, pk=pk)
+        serializer = RequestSerializer(r)
+        return Response(serializer.data)
