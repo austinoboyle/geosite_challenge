1c59a555cae7537e4a7e1a856d05806f8c86b60b
Austin O'Boyle <14arob@queensu.ca>
2019-10-27 22:53:00 -0700

    Define the 'Request' model
    Contains created_at, request_type, and comment fields

--- api/models.py
+++ api/models.py
@@ -1,1 +1,32 @@
 from django.db import models
+from django.db import models
+from enum import Enum
+
+
+class RequestType(Enum):
+    """
+    Enumerated HTTP request types.
+    """
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    OPTIONS = "OPTIONS"
+
+    @classmethod
+    def choices(cls):
+        return [(req.name, req.value) for req in cls]
+
+
+class Request(models.Model):
+    """
+    Model to capture HTTP requests made to the API.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    request_type = models.CharField(
+        max_length=10, choices=RequestType.choices(), null=False, blank=False)
+    comment = models.CharField(max_length=255, null=True)
+
+    class Meta:
+
+        ordering = ['-created_at']
